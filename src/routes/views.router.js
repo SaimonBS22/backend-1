5fe304b7fd07e28b526b6e8e5d01ed4e73731968
simon blaksley <simonblaksley@gmail.com>
2025-01-11 21:28:06 -0300

    haciendo el real time products

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import ProductManager from "../manager/productManager.js";
 
 const viewsRouter = Router()
+const manager = new ProductManager('./src/data/products.json')
 
-viewsRouter.get('/', (req,res)=>{
-    res.render('home')
+
+viewsRouter.get('/products', async (req,res)=>{
+    const productos = await manager.getProducts()
+    res.render('home', {productos})
+    
+})
+
+viewsRouter.get('/realtimeproducts', async (req, res)=>{
+    res.render('realtimeproducts')
 })
 
 export default viewsRouter
